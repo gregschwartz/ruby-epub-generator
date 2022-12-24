0b9189092c325b6b29378782291023273afe9e49
Greg Schwartz <greg@gregschwartz.net>
2022-12-24 10:42:54 -0800

    Exporting and storage are working!

--- generate.rb
+++ generate.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
+require 'json'
 require 'mechanize'
 require 'gepub'
+
+URL_STORAGE = "urls.json"
+wordsToConsiderNext = ["Next"]#, ">", "Continue"]
 
 def cleanHTML(html)
   html.gsub!(/<hr [^\/>]+>/, "<hr \/>")
@@ -8,25 +12,84 @@ def cleanHTML(html)
   return html
 end
 
-def addChapter(chapterNumber, title, content)
-  content = "<html xmlns='http://www.w3.org/1999/xhtml'><body>" + cleanHTML(content) + "</body></html>"
-
-  book.add_item("text/chap#{chapterNumber}.xhtml").add_content(StringIO.new(content)).toc_text(title).landmark(type: "bodymatter", title: title)
+##### Load stored URLs from last time
+begin
+  rawInput = File.read(URL_STORAGE)
+rescue Errno::ENOENT
+  puts "File #{URL_STORAGE} not found. Please create it."
+  return
+rescue Errno::EACCES
+  puts "Insufficient permissions, not allowed to open file #{URL_STORAGE}."
+  return
+rescue => r
+  puts "Error loading #{URL_STORAGE}: #{r}"
+  return
+end
+unless rawInput && rawInput.length > 0
+  puts "Error, cannot find file #{URL_STORAGE} or is empty"
+  return
 end
 
+begin
+  urls = JSON.parse(rawInput)
+rescue JSON::ParserError
+  puts "File #{URL_STORAGE} isn't valid JSON"
+  return
+rescue r
+  puts "Error parsing #{URL_STORAGE} into JSON: #{r}"
+  return
+end
+unless urls && urls.length > 0
+  puts "Error, file #{URL_STORAGE} is empty"
+  return
+end
+
+
+#### Overrides
+isFirstTimeRunning = true
 urls = [
-  "https://www.reddit.com/r/redditserials/comments/n1duqa/leveling_up_the_world_adventure_arc_chapter_119/"
+  # "https://www.reddit.com/r/HFY/comments/z37kbd/the_great_erectus_and_faun_romance/", #last one, no next link
+
+  #already caught up on
+  # "https://www.reddit.com/r/HFY/comments/xsmwaa/the_great_erectus_and_faun_poop_and_circumstance/",
+  # "https://www.reddit.com/r/HFY/comments/xsux21/eagle_springs_stories_truthseeker_chapter_0/",
+  # "https://www.reddit.com/r/HFY/comments/xsxome/dungeon_tour_guide_ch_24/",
+  # "https://www.reddit.com/r/HFY/comments/ynuook/we_need_a_deathworlder_pt51/",
+  # "https://www.reddit.com/r/HFY/comments/xu3y50/of_men_and_dragons_book_3_chapter_15/",
+  # "https://www.reddit.com/r/HFY/comments/xu6mfj/transferred_chapter_16/",
+  # "https://www.reddit.com/r/HFY/comments/xukozz/the_nature_of_predators_51/",
+  # "https://www.reddit.com/r/HFY/comments/xutaig/finding_posella_34/",
+  # "https://www.reddit.com/r/HFY/comments/xzyl2l/gods_saviors_people_part_20_a_journey_of_a/",
+  # "https://www.reddit.com/r/HFY/comments/xz9u43/humans_dont_make_good_familiars_book_2_part_18/",
+  # "https://www.reddit.com/r/HFY/comments/xy2q3u/no_simple_beast_episode_6/",
+  # "https://www.reddit.com/r/HFY/comments/xx461i/eagle_springs_stories_testing_the_waters_part_2/",
+  # "https://www.reddit.com/r/HFY/comments/xx3ldo/extermination_order_23_hunters_shutins_and/",
+  # "https://www.reddit.com/r/HFY/comments/xwn2pq/a_teenage_death_commando_goes_to_school_the_final/",
+  # "https://www.reddit.com/r/HFY/comments/xvfr98/a_job_for_a_deathworlder_chapter_83/",
+  # "https://www.reddit.com/r/HFY/comments/y532g6/tales_from_the_terran_republic_demonstrations/",
+  # "https://www.reddit.com/r/HFY/comments/y3fj6f/hallows_8_the_disturbance/"
+
+  #live series
+
+  #not on reddit, but try
+  #"https://theyaresmol.com/smolive-garden-chapter-16-the-absolute-and-most-correct-choice/",
+
+  # "https://www.reddit.com/r/redditserials/comments/n1duqa/leveling_up_the_world_adventure_arc_chapter_119/", #huge series
 ]
 
+###########
+
+urlsToStoreAndCheckNextTime = []
 
 #filename is the date and time
-dateAsString = Time.new.strftime("%Y-%m-%d_%H-%M-%S")
+dateForFilename = Time.new.strftime("%Y-%m-%d_%H-%M-%S")
+dateForTitle = Time.new.strftime("%m-%d_%H-%M")
 
 book = GEPUB::Book.new
-book.primary_identifier('http://gregschwartz.net', dateAsString, 'URL')
+book.primary_identifier('http://gregschwartz.net', dateForFilename, 'URL')
 book.language = 'en'
 
-book.add_title "Subscriptions #{dateAsString}",
+book.add_title "Subscriptions #{dateForTitle}",
                title_type: GEPUB::TITLE_TYPE::MAIN,
                lang: 'en',
                file_as: "Greg's Subscriptions",
@@ -35,10 +98,15 @@ book.add_title "Subscriptions #{dateAsString}",
 book.add_creator 'Greg',
                  display_seq:1
 
+#make sure the words are lowercase
+wordsToConsiderNext.map!{|w| w.downcase}
+
 #web connection
 a = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
 }
+
+
 
 #add items
 book.ordered do
@@ -50,34 +118,76 @@ book.ordered do
                   <title>cover page</title>
                 </head>
                 <body>
-                <h1>#{dateAsString}</h1>
+                <h1>#{dateForTitle}</h1>
                 </body></html>
   COVER
 
-  chapterNumber = 1
+  seriesNumber = 0
   urls.each do |url|
+    #normally the stored URL was the last one we read, so we don't want to include it
+    addToExport = isFirstTimeRunning #false
+    ###### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ###### TODO: remove isFirstTimeRunning and overriding urls !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ###### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
     #load the URL, and loop if there is a Next link
-    loop do
-      puts "Load #{url}"
-      a.get(url) do |page|
-        title = page.title
-        contentDiv = page.search(".RichTextJSON-root")
+    puts "\nSeries: #{url}"
+    chapterNumber = 1
+    a.get(url) do |page|
+      loop do #loop to iterate on this URL and "next" pages
+        puts "Page: #{page.uri}"
 
-        addChapter(chapterNumber, title, contentDiv.map{|elt| elt.to_s}.first)
-        nextLink = contentDiv.search("a")
+        if (addToExport)
+          title = page.title
 
-        puts "\tAdded as chapter"
-      end
+          content = cleanHTML(page.at(".RichTextJSON-root").to_s)
+          contentWrapped = "<html xmlns='http://www.w3.org/1999/xhtml'><body><h1>"+ title +"</h1><p><a href='#{page.uri}'>#{page.uri}</a></p>" + content + "</body></html>"
 
-      chapterNumber += 1
-      sleep(rand(2))
+          book.add_item("text/s#{seriesNumber}/ch#{chapterNumber}.xhtml").add_content(StringIO.new(contentWrapped)).toc_text(title).landmark(type: "bodymatter", title: title)
 
+          puts "\tAdded as chapter (#{content.length} chars)"
+        end
 
-      break unless nextLink
-    end #loop
+        #### Look for link to next story in series
+        nextLink = nil
+        links = page.links #because Mechanize SUCKS and cannot narrow down links further, argh
+        links.each do |link|
+          #see if any of the wordsToConsiderNext appear in the link text
+          if wordsToConsiderNext.select{|word| link.text.downcase.match(word)}.length > 0
+            #puts "\tNext link: #{link.href}"
+            nextLink = link
+            break
+          end
+        end
+        break unless nextLink
+
+        chapterNumber += 1
+        sleep(rand(2))
+
+        #testing
+        break if chapterNumber > 30
+
+        #click on next link and repeat to grab
+        page = nextLink.click
+
+        #now we include every page we process
+        addToExport = true
+      end #loop
+
+      #store current URL as the one to start with next time
+      urlsToStoreAndCheckNextTime.push(page.uri)
+      puts "\tSaved URL for next time: #{page.uri}"
+
+      seriesNumber += 1
+    end #a.get(url)
   end #urls.each
 end #book.ordered
 
-epubname = File.join(File.dirname(__FILE__), dateAsString + ".epub")
+#Store URLs to disk
+File.open(URL_STORAGE,"w") { |f| f.write(urlsToStoreAndCheckNextTime.to_json) }
 
+#Output epub
+epubname = File.join(File.dirname(__FILE__), dateForFilename + ".epub")
 book.generate_epub(epubname)
+puts "epub exported!"
